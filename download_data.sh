f6eec9b2c12fa119f75d8fcf4eb3a089882b2c26
Lukáš Hudec <xhudec2@fi.muni.cz>
2026-02-23 21:09:45 +0100

    update download script

--- download_data.sh
+++ download_data.sh
@@ -7,7 +7,7 @@ gdown "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/Lipophilicity.cs
 
 # takes around 5-10 minutes
 git clone https://github.com/davidbuterez/mf-pcba.git
-python mf-pcba/pubchem_retrieve.py --AID "504329" --list_of_sd_cols "Primary Screen Percent Inhibition @ 12.5 uM Rep 1" "Primary Screen Percent Inhibition @ 12.5 uM Rep 2" "Primary Screen Percent Inhibition @ 12.5 uM Rep 3" --list_of_dr_cols "IC50" --transform_dr "pXC50" --save_dir data
+uv run mf-pcba/pubchem_retrieve.py --AID "504329" --list_of_sd_cols "Primary Screen Percent Inhibition @ 12.5 uM Rep 1" "Primary Screen Percent Inhibition @ 12.5 uM Rep 2" "Primary Screen Percent Inhibition @ 12.5 uM Rep 3" --list_of_dr_cols "IC50" --transform_dr "pXC50" --save_dir data
 
 mv data/AID504329/SD.csv data/SD.csv
 # cleanup
